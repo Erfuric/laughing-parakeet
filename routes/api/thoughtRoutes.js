c89b5a2b20c0ff8060379b1d7bf60a4bf2b42cc1
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-04-08 16:07:28 +1000

    routes updated and mostly done

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -23,3 +23,35 @@
 
 ---
 */
+const router = require('express').Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+router
+  .route('/thoughts')
+  .get(getAllThoughts)
+  .post(createThought);
+
+router
+  .route('/thoughts/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router
+  .route('/thoughts/:thoughtId/reactions')
+  .post(createReaction);
+
+router
+  .route('/thoughts/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
+module.exports = router;
